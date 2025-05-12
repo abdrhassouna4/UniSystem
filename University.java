a426de4dfd5beb42e8fe43013218c8a20b67e149
abdrhassouna4 <cds.AbdelrahmanAhmed250209@alexu.edu.eg>
2025-05-12 19:49:04 +0300

    Update University.java

--- University.java
+++ University.java
@@ -1,63 +1,47 @@
-import java.util.*;
-
 public class University {
-    private String name;
-    private List<Student> students;
-    private List<Faculty> facultyMembers;
-    private List<Department> departments;
-    private List<Course> courses;
+    private Department[] departments;
+    private User[] users;
+    private Course[] courses;
+    private String academicCalendar;
 
-    // Constructor for the University class
-    public University(String name) {
-        this.name = name;
-        this.students = new ArrayList<>();
-        this.facultyMembers = new ArrayList<>();
-        this.departments = new ArrayList<>();
-        this.courses = new ArrayList<>();
+    private int departmentCount;
+    private int userCount;
+    private int courseCount;
+
+    public University() {
+        this.departments = new Department[50]; // Initial capacity for departments
+        this.users = new User[200];            // Initial capacity for users
+        this.courses = new Course[200];        // Initial capacity for courses
+        this.departmentCount = 0;
+        this.userCount = 0;
+        this.courseCount = 0;
+        this.academicCalendar = "";
     }
 
-    // ✅ Register a new student in the university
     public void registerStudent(Student student) {
-        students.add(student);
-        System.out.println("Student " + student.getName() + " has been registered.");
+        if (userCount < users.length) {
+            users[userCount++] = student;
+            System.out.println("Student registered.");
+        } else {
+            System.out.println("Cannot register more users.");
+        }
     }
 
-    // ✅ Hire a new faculty member
-    public void hireFaculty(Faculty faculty) {
-        facultyMembers.add(faculty);
-        System.out.println("Faculty member " + faculty.getName() + " has been hired.");
+    public void hireFaculty(Instructor instructor) {
+        if (userCount < users.length) {
+            users[userCount++] = instructor;
+            System.out.println("Faculty hired.");
+        } else {
+            System.out.println("Cannot hire more users.");
+        }
     }
 
-    // ✅ Create a new department
     public void createDepartment(Department department) {
-        departments.add(department);
-        System.out.println("Department " + department.getDepartmentName() + " has been created.");
-    }
-
-    // ✅ Offer a new course at the university
-    public void offerCourse(Course course) {
-        courses.add(course);
-        System.out.println("Course " + course.getTitle() + " has been offered.");
-    }
-
-    // Getter methods for university attributes
-    public String getName() {
-        return name;
-    }
-
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public List<Faculty> getFacultyMembers() {
-        return facultyMembers;
-    }
-
-    public List<Department> getDepartments() {
-        return departments;
-    }
-
-    public List<Course> getCourses() {
-        return courses;
+        if (departmentCount < departments.length) {
+            departments[departmentCount++] = department;
+            System.out.println("Department created.");
+        } else {
+            System.out.println("Cannot add more departments.");
+        }
     }
 }
