51699555d76251556faf2c5aa282d55715ec4681
abdrhassouna4 <cds.AbdelrahmanAhmed250209@alexu.edu.eg>
2025-05-12 19:47:54 +0300

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,51 +1,69 @@
-import java.util.*;
-
 public class Department {
-    private String departmentName;
-    private List<Faculty> facultyList;
-    private List<Course> courseList;
+    private String departmentId;
+    private String name;
+    private Instructor[] faculty;
+    private Course[] offeredCourses;
+    private int facultyCount;
+    private int courseCount;
 
-    // Constructor to initialize the department
-    public Department(String departmentName) {
-        this.departmentName = departmentName;
-        this.facultyList = new ArrayList<>();
-        this.courseList = new ArrayList<>();
+    public Department(String departmentId, String name) {
+        this.departmentId = departmentId;
+        this.name = name;
+        this.faculty = new Instructor[100]; // Initial capacity for faculty
+        this.offeredCourses = new Course[100]; // Initial capacity for courses
+        this.facultyCount = 0;
+        this.courseCount = 0;
     }
 
-    // ✅ Method to add a faculty member to the department
-    public void addFaculty(Faculty faculty) {
-        facultyList.add(faculty);
-        System.out.println("Faculty " + faculty.getName() + " added to the department: " + departmentName);
-    }
-
-    // ✅ Method to remove a faculty member from the department
-    public void removeFaculty(Faculty faculty) {
-        if (facultyList.contains(faculty)) {
-            facultyList.remove(faculty);
-            System.out.println("Faculty " + faculty.getName() + " removed from the department: " + departmentName);
+    public void addFaculty(Instructor instructor) {
+        if (facultyCount < faculty.length) {
+            faculty[facultyCount++] = instructor;
+            System.out.println("Faculty member added to department.");
         } else {
-            System.out.println("Faculty " + faculty.getName() + " not found in the department: " + departmentName);
+            System.out.println("Cannot add more faculty members.");
         }
     }
 
-    // ✅ Method to add a course to the department
+    public void removeFaculty(String instructorId) {
+        boolean found = false;
+        for (int i = 0; i < facultyCount; i++) {
+            if (faculty[i].getUserId().equals(instructorId)) {
+                for (int j = i; j < facultyCount - 1; j++) {
+                    faculty[j] = faculty[j + 1];
+                }
+                faculty[facultyCount - 1] = null;
+                facultyCount--;
+                found = true;
+                System.out.println("Faculty member removed.");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Faculty member not found.");
+        }
+    }
+
     public void addCourse(Course course) {
-        courseList.add(course);
-        System.out.println("Course " + course.getTitle() + " added to the department: " + departmentName);
+        if (courseCount < offeredCourses.length) {
+            offeredCourses[courseCount++] = course;
+            System.out.println("Course added to department.");
+        } else {
+            System.out.println("Cannot add more courses.");
+        }
     }
 
-    // ✅ Method to get the list of faculty members in the department
-    public List<Faculty> getFacultyList() {
-        return facultyList;
+    public void getFacultyList() {
+        System.out.println("Faculty in " + name + " Department:");
+        for (int i = 0; i < facultyCount; i++) {
+            System.out.println("- " + faculty[i].getName() + " (" + faculty[i].getUserId() + ")");
+        }
     }
 
-    // ✅ Method to get the list of courses offered by the department
-    public List<Course> getCourseList() {
-        return courseList;
+    public String getDepartmentId() {
+        return departmentId;
     }
 
-    // ✅ Getter method for department name
-    public String getDepartmentName() {
-        return departmentName;
+    public String getName() {
+        return name;
     }
 }
